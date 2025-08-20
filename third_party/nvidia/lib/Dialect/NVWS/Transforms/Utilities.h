972021ed3e7d962d8f2c6364f2ab6cba20674d43
Evghenii <egaburov@nvidia.com>
2025-08-20 13:04:38 +0000

    re-order aref-lower & code-split

--- third_party/nvidia/lib/Dialect/NVWS/Transforms/Utilities.h
+++ third_party/nvidia/lib/Dialect/NVWS/Transforms/Utilities.h
@@ -12,6 +12,16 @@ Operation *createAlloc(OpBuilder &builder, Location loc,
 ArefCreateOp createArefCreateOp(OpBuilder &builder, ArrayRef<Type> arefTypes,
                                 ValueRange allocOps, Location loc);
 
+                                template <typename Range>
+inline std::optional<int> findValuePosInRange(const Range &range,
+                                              mlir::Value v) {
+  for (auto [pos, arg] : llvm::enumerate(range)) {
+    if (arg == v)
+      return pos;
+  }
+  return {};
+}
+
 } // namespace mlir::triton::nvws
 
 #endif // NVIDIA_NVWS_TRANSFORMS_UTILITY_H_
