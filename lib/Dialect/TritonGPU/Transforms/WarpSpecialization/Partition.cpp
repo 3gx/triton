c8d578398c2014b27af5c7644fe6fae31a074753
Evghenii <egaburov@nvidia.com>
2025-09-09 22:54:40 +0000

    masa-partition-rework squash

--- lib/Dialect/TritonGPU/Transforms/WarpSpecialization/Partition.cpp
+++ lib/Dialect/TritonGPU/Transforms/WarpSpecialization/Partition.cpp
@@ -1,7 +1,9 @@
 #include "triton/Dialect/TritonGPU/Transforms/Partition.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "triton/Dialect/TritonGPU/Transforms/PipeliningUtility.h"
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
 #include "llvm/ADT/SCCIterator.h"
+#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/Use.h"
 
 using namespace mlir;
@@ -9,46 +11,22 @@ using namespace triton;
 using namespace triton::gpu;
 
 //===----------------------------------------------------------------------===//
-// WarpSchedule
+// PartitionSet
 //===----------------------------------------------------------------------===//
 
-Partition *WarpSchedule::addPartition(unsigned stage) {
+Partition *PartitionSet::addPartition(unsigned stage) {
   partitions.push_back(std::make_unique<Partition>(partitions.size(), stage));
   return partitions.back().get();
 }
 
-Partition *WarpSchedule::getPartition(Operation *op) {
-  return opToPartition.lookup(op);
-}
-const Partition *WarpSchedule::getPartition(Operation *op) const {
-  return opToPartition.lookup(op);
-}
-
-Partition *WarpSchedule::getPartition(unsigned idx) {
+Partition *PartitionSet::getPartition(unsigned idx) {
   return partitions[idx].get();
 }
-const Partition *WarpSchedule::getPartition(unsigned idx) const {
+const Partition *PartitionSet::getPartition(unsigned idx) const {
   return partitions[idx].get();
 }
 
-void WarpSchedule::insert(Partition *partition, Operation *op) {
-  partition->ops.push_back(op);
-  opToPartition[op] = partition;
-}
-
-bool WarpSchedule::isScheduled(Operation *op) const {
-  const Partition *partition = getPartition(op);
-  return partition && partition != getRootPartition();
-}
-
-bool WarpSchedule::trySchedule(Partition *partition, Operation *op) {
-  if (isScheduled(op))
-    return false;
-  insert(partition, op);
-  return true;
-}
-
-FailureOr<WarpSchedule> WarpSchedule::deserialize(scf::ForOp loop) {
+FailureOr<PartitionSet> PartitionSet::deserialize(scf::ForOp loop) {
   auto stages = loop->getAttrOfType<ArrayAttr>(kPartitionStagesAttrName);
   if (!stages)
     return failure();
@@ -57,7 +35,7 @@ FailureOr<WarpSchedule> WarpSchedule::deserialize(scf::ForOp loop) {
   if (!tag)
     return failure();
 
-  WarpSchedule result;
+  PartitionSet result;
   result.tag = tag.getInt();
   for (auto [idx, attr] : llvm::enumerate(stages)) {
     auto stage = dyn_cast<IntegerAttr>(attr);
@@ -71,73 +49,82 @@ FailureOr<WarpSchedule> WarpSchedule::deserialize(scf::ForOp loop) {
   }
 
   for (Operation &op : loop.getBody()->without_terminator()) {
-    Partition *partition = result.getRootPartition();
-    if (auto attr = op.getAttrOfType<IntegerAttr>(kPartitionAttrName)) {
-      int64_t idx = attr.getInt();
-      if (idx < 0 || idx >= result.partitions.size())
-        return mlir::emitError(op.getLoc(), "invalid partition index ") << idx;
-      partition = result.partitions[idx].get();
+    if (auto attrs = getPartitionIds(&op)) {
+      for (auto idx : *attrs) {
+        if (idx < 0 || idx >= result.partitions.size())
+          return mlir::emitError(op.getLoc(), "invalid partition index ")
+                 << idx;
+        result.partitions[idx]->addOp(&op);
+      }
     }
-    result.insert(partition, &op);
   }
 
   return result;
 }
 
-void WarpSchedule::serialize(scf::ForOp loop) const {
-  SmallVector<Attribute> stages;
-  Builder b(loop.getContext());
-  for (Operation &op : loop.getBody()->without_terminator()) {
-    if (Partition *partition = opToPartition.lookup(&op)) {
-      if (partition == getRootPartition())
-        continue;
-      op.setAttr(kPartitionAttrName,
-                 b.getI32IntegerAttr(partition->getIndex()));
+void PartitionSet::dump() const {
+  for (auto [i, partition] :
+       llvm::enumerate(llvm::make_pointee_range(partitions))) {
+    llvm::errs() << "=== PARTITION #" << i << " ===\n";
+    for (Operation *op : partition.getOps()) {
+      op->print(llvm::errs(), OpPrintingFlags().skipRegions());
+      llvm::errs() << "\n";
     }
+    llvm::errs() << "\n";
   }
-  for (Partition &partition : getPartitions())
-    stages.push_back(b.getI32IntegerAttr(partition.getStage()));
-  loop->setAttr(kPartitionStagesAttrName, b.getArrayAttr(stages));
+  llvm::errs() << "\n";
 }
 
-LogicalResult WarpSchedule::verify(scf::ForOp loop) const {
-  // The root partition is only allowed to transitively depend on itself.
-  bool failed = false;
-  iterateInputs(loop, getRootPartition(), [&](OpOperand &input) {
-    auto [def, distance] = getDefiningOpAndDistance(loop, input.get());
-    // Ignore values defined outside the loop.
-    if (!def || def->getParentOp() != loop)
-      return;
-    const Partition *defPartition = opToPartition.at(def);
-    if (defPartition == getRootPartition())
-      return;
-    InFlightDiagnostic diag = mlir::emitWarning(input.getOwner()->getLoc());
-    diag << "operation in the root partition depends on a value that "
-            "originates from a non-root partition through operand #"
-         << input.getOperandNumber();
-    diag.attachNote(def->getLoc())
-        << "operand defined here in partition #" << defPartition->getIndex()
-        << " at distance " << distance;
-    failed = true;
-  });
-  if (failed)
-    return failure();
+namespace mlir::triton::gpu {
 
-  return success();
+void setPartition(Operation *op, ArrayRef<int> partitionIds) {
+  Builder b(op->getContext());
+  op->setAttr(kPartitionAttrName, b.getDenseI32ArrayAttr(partitionIds));
 }
 
-void WarpSchedule::eraseFrom(scf::ForOp loop) {
-  loop.walk([&](Operation *op) { op->removeAttr(kPartitionAttrName); });
-  loop->removeAttr(kPartitionStagesAttrName);
+void setPartition(Operation *op, const SetVector<int> &partitionIds) {
+  SmallVector<int> partitions(partitionIds.begin(), partitionIds.end());
+  setPartition(op, partitions);
 }
 
-void WarpSchedule::iterateInputs(
-    scf::ForOp loop, const Partition *partition,
-    function_ref<void(OpOperand &)> callback) const {
+void setPartition(Operation *op, Partition *partition) {
+  if (op->getAttr(kPartitionAttrName)) {
+    // Allow overwriting in this case
+    // TODO: is this the right thing to do
+  }
+
+  SmallVector<int> partitions{partition->getIndex()};
+  setPartition(op, partitions);
+  partition->addOp(op);
+}
+
+std::optional<SetVector<int>> getPartitionIds(Operation *op) {
+  if (!op) {
+    return std::nullopt;
+  }
+  auto attrs = op->getAttr(kPartitionAttrName);
+  if (!attrs) {
+    return std::nullopt;
+  }
+  if (!isa<DenseI32ArrayAttr>(attrs)) {
+    op->dump();
+  }
+  SetVector<int> partitionIds;
+  for (auto id : cast<DenseI32ArrayAttr>(attrs).asArrayRef()) {
+    partitionIds.insert(id);
+  }
+  return partitionIds;
+}
+
+bool hasPartition(Operation *op) { return getPartitionIds(op) != std::nullopt; }
+
+void iterateInputs(scf::ForOp loop, const Partition *partition,
+                   function_ref<void(OpOperand &)> callback) {
   for (Operation *op : partition->getOps()) {
     visitNestedOperands(op, [&](OpOperand &operand) {
       // Ignore implicit captures.
       Value value = operand.get();
+      auto partitionIds = getPartitionIds(value.getDefiningOp());
       if (value.getParentBlock() != loop.getBody())
         return;
       if (auto arg = dyn_cast<BlockArgument>(value)) {
@@ -148,7 +135,8 @@ void WarpSchedule::iterateInputs(
         // This value originates from a previous iteration.
         assert(llvm::is_contained(loop.getRegionIterArgs(), arg));
         callback(operand);
-      } else if (getPartition(value.getDefiningOp()) != partition) {
+      } else if (!partitionIds ||
+                 !llvm::is_contained(*partitionIds, partition->getIndex())) {
         // This value originates from a different partition in the same
         // iteration.
         assert(value.getDefiningOp()->getParentOp() == loop);
@@ -158,16 +146,17 @@ void WarpSchedule::iterateInputs(
   }
 }
 
-void WarpSchedule::iterateOutputs(
-    scf::ForOp loop, const Partition *partition,
-    function_ref<void(Operation *, OpOperand &)> callback) const {
+void iterateOutputs(scf::ForOp loop, const Partition *partition,
+                    function_ref<void(Operation *, OpOperand &)> callback) {
   for (Operation *op : partition->getOps()) {
     for (OpOperand &use : op->getUses()) {
       Operation *owner = loop.getBody()->findAncestorOpInBlock(*use.getOwner());
+      auto partitionIds = getPartitionIds(owner);
       if (isa<scf::YieldOp>(owner)) {
         // This value is used in a subsequent iteration.
         callback(owner, use);
-      } else if (getPartition(owner) != partition) {
+      } else if (!partitionIds ||
+                 !llvm::is_contained(*partitionIds, partition->getIndex())) {
         // This value is used in a different partition in the same iteration.
         callback(owner, use);
       }
@@ -175,9 +164,8 @@ void WarpSchedule::iterateOutputs(
   }
 }
 
-void WarpSchedule::iterateDefs(
-    scf::ForOp loop, const Partition *partition,
-    function_ref<void(OpResult, unsigned)> callback) const {
+void iterateDefs(scf::ForOp loop, const Partition *partition,
+                 function_ref<void(OpResult, unsigned)> callback) {
   iterateInputs(loop, partition, [&](OpOperand &input) {
     auto [def, distance] = getDefinitionAndDistance(loop, input.get());
     if (def && def.getParentBlock() == loop.getBody())
@@ -185,9 +173,8 @@ void WarpSchedule::iterateDefs(
   });
 }
 
-void WarpSchedule::iterateUses(
-    scf::ForOp loop, const Partition *partition,
-    function_ref<void(OpResult, OpOperand &, unsigned)> callback) const {
+void iterateUses(scf::ForOp loop, const Partition *partition,
+                 function_ref<void(OpResult, OpOperand &, unsigned)> callback) {
   SmallVector<std::tuple<OpResult, OpOperand *, unsigned>> uses;
   iterateOutputs(loop, partition, [&](Operation *owner, OpOperand &use) {
     uses.emplace_back(cast<OpResult>(use.get()), &use, 0);
@@ -205,20 +192,10 @@ void WarpSchedule::iterateUses(
   }
 }
 
-void WarpSchedule::dump() const {
-  for (auto [i, partition] :
-       llvm::enumerate(llvm::make_pointee_range(partitions))) {
-    llvm::errs() << "=== PARTITION #" << i << " ===\n";
-    for (Operation *op : partition.getOps()) {
-      op->print(llvm::errs(), OpPrintingFlags().skipRegions());
-      llvm::errs() << "\n";
-    }
-    llvm::errs() << "\n";
-  }
-  llvm::errs() << "=== ROOT PARTITION ===\n";
-  for (Operation *op : getRootPartition()->getOps()) {
-    op->print(llvm::errs(), OpPrintingFlags().skipRegions());
-    llvm::errs() << "\n";
-  }
-  llvm::errs() << "\n";
+Partition *getPartition(Operation *op, PartitionSet &partitions) {
+  auto id = getPartitionIds(op);
+  assert(id && id->size() == 1);
+  return partitions.getPartition((*id)[0]);
 }
+
+} // namespace mlir::triton::gpu
