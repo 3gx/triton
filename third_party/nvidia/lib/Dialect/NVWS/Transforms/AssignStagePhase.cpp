0954cd4ca01e0869740a98ab78e5ed0681d2d156
Evghenii <egaburov@nvidia.com>
2025-11-04 13:48:36 +0000

    fix assing-stage to propagate to control

--- third_party/nvidia/lib/Dialect/NVWS/Transforms/AssignStagePhase.cpp
+++ third_party/nvidia/lib/Dialect/NVWS/Transforms/AssignStagePhase.cpp
@@ -446,7 +446,13 @@ void visitBackwardSlice(scf::ForOp wsLoop, Value value,
         return;
       auto pos = findValuePosInRange(forOp.getRegionIterArgs(), value);
       assert(pos);
+      auto partitionIds = *getPartitionIds(forOp);
       visitBackwardSlice(wsLoop, forOp.getInitArgs()[*pos], callback, visited);
+      // visit control operands of for-op
+      for (int idx = 0; idx < forOp.getNumControlOperands(); ++idx) {
+        auto control = forOp.getOperand(idx);
+        visitBackwardSlice(wsLoop, control, callback, visited);
+      }
     }
   } else if (auto defOp = value.getDefiningOp();
              isa<scf::IfOp, scf::ForOp>(defOp)) {
